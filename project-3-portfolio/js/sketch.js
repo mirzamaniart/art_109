5bcd37c39647bc9bfdb0f8bbd0542cc33433d97c
Jasmine Mirzamani <128877000+mirzamaniart@users.noreply.github.com>
2025-04-29 11:39:26 -0700

    Add files via upload

---
+++ project-3-portfolio/js/sketch.js
@@ -0,0 +1,28 @@
+// trail array
+let trail = [];
+
+// setup canvas
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noCursor();
+}
+
+// draw animation
+function draw() {
+  background(0, 0, 0, 10);
+
+  let pos = createVector(mouseX, mouseY);
+  trail.push(pos);
+
+  if (trail.length > 50) {
+    trail.splice(0, 1);
+  }
+
+  noStroke();
+  fill(128, 0, 128);
+
+  for (let i = 0; i < trail.length; i++) {
+    let p = trail[i];
+    ellipse(p.x, p.y, 20, 20);
+  }
+}
